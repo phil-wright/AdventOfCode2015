31a8b70027e02a59bc649b5a9e25af4ca177db4f
Phil Wright <Phil.Wright@cegedimrx.co.uk>
2018-02-01 12:29:20 +0000

    Complete Part 2

--- day02.fsx
+++ day02.fsx
@@ -31,3 +31,26 @@ open System.IO
 let file = Path.Combine(__SOURCE_DIRECTORY__, "day02.txt")
 File.ReadAllLines(file)
 |> Seq.sumBy paperNeeded
+
+let perimeter a b =
+    2*a + 2*b
+
+let ribbonNeeded  (input : string) =
+    let parseDimensions (input : string) (splitBy : char) =
+        input |> split splitBy |> Array.map int
+
+    let [| length; width; height |] = parseDimensions input 'x'
+    let p1 = perimeter length width
+    let p2 = perimeter width height
+    let p3 = perimeter length height
+    let perimeter = List.min [ p1; p2; p3 ]
+    let bowRibbon = length * width * height
+    perimeter + bowRibbon
+
+ribbonNeeded "2x3x4"
+ribbonNeeded "1x1x10"
+
+open System.IO
+let file = Path.Combine(__SOURCE_DIRECTORY__, "day02.txt")
+File.ReadAllLines(file)
+|> Seq.sumBy ribbonNeeded
