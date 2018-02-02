55f3a54c4dc08d7cd0d30640ec1f9a3003e7752f
Phil Wright <Phil.Wright@cegedimrx.co.uk>
2018-02-02 17:06:18 +0000

    Day 03 - Part 2

--- day03.fsx
+++ day03.fsx
@@ -13,17 +13,50 @@ let updateCount (tally : Map<(int * int), int>) house =
         tempTally.Add (house, numberOfPresents+1)
     | None -> tally.Add (house, 1)
 
-let input = ">"
-let input = "<>"
-let input = "^>v<"
-let input = "^v^v^v^v^v"
+let input1 = ">"
+let input1 = "<>"
+let input1 = "^>v<"
+let input1 = "^v^v^v^v^v"
 
 open System.IO
 let file = Path.Combine(__SOURCE_DIRECTORY__, "day03.txt")
-let input = File.ReadAllText file
+let input1 = File.ReadAllText file
 
-input
+input1
 |> Seq.scan move (0,0)
 |> Seq.fold updateCount Map.empty
-|> Map.filter (fun _ n -> n > 1)
 |> Seq.length
+
+// Part 2
+let santaMoves (l : string) =
+    let indexes = [0..2..l.Length-1]
+    indexes
+    |> Seq.map (fun n -> l.[n])
+    |> List.ofSeq
+
+let roboSantaMoves (l : string) =
+    let indexes = [1..2..l.Length-1]
+    indexes
+    |> Seq.map (fun n -> l.[n])
+    |> List.ofSeq
+
+let input2 = "^v"
+let input2 = "^v^v^v^v^v"
+let input2 = File.ReadAllText file
+
+input2 |> santaMoves
+input2 |> roboSantaMoves
+
+let presents presentTally input =
+    input
+    |> Seq.scan move (0,0)
+    |> Seq.fold updateCount presentTally
+
+let santaPresents presentTally input =
+    input |> santaMoves |> presents presentTally
+
+let roboSantaPresents presentTally input =
+    input |> roboSantaMoves |> presents presentTally
+
+let presentsFromSanta = input2 |> santaPresents (Map.empty.Add((0,0), 1))
+input2 |> roboSantaPresents presentsFromSanta |> Seq.length
